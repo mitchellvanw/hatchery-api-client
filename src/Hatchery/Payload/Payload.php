e01468a051dc092bfd3897ee6866d184f9f2c254
Mitchell van Wijngaarden <mitchell@kooding.nl>
2015-02-16 16:54:11 +0100

    Change Payload to an interface

--- src/Hatchery/Payload/Payload.php
+++ src/Hatchery/Payload/Payload.php
@@ -2,62 +2,9 @@
 
 namespace Hatchery\Payload;
 
-abstract class Payload
+interface Payload
 {
-
-    protected $data = array();
-    protected $headers = array();
-    protected $url;
-    protected $method = 'post';
-
-    public function __construct($url)
-    {
-        $this->url = $url;
-    }
-
-    public function setUrl($url)
-    {
-        $this->url = $url;
-        return $this;
-    }
-
-    public function getUrl()
-    {
-        return $this->url;
-    }
-
-    public function setHeader($header, $value)
-    {
-        $this->headers[$header] = $value;
-        return $this;
-    }
-
-    public function getHeaders()
-    {
-        return $this->headers;
-    }
-
-    public function setPostData($key, $value)
-    {
-        $this->data[$key] = $value;
-        return $this;
-    }
-
-    public function getPostData()
-    {
-        return $this->data;
-    }
-
-    public function setMethod($method)
-    {
-
-        $this->method = $method;
-        return $this;
-    }
-
-    public function getMethod()
-    {
-
-        return $this->method;
-    }
+    public function getVerb();
+    public function getUri();
+    public function getHeaders();
 }
