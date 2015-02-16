b2c5e4d83a3fa86457c6a0407e405056949f0619
Mitchell van Wijngaarden <mitchell@kooding.nl>
2015-02-16 17:42:07 +0100

    Some style changes

--- src/Hatchery/Connection/Curl/CurlPost.php
+++ src/Hatchery/Connection/Curl/CurlPost.php
@@ -5,23 +5,21 @@ namespace Hatchery\Connection\Curl;
 use Hatchery\Connection\TypeInterface;
 use Hatchery\Payload\Payload;
 
-class CurlPost implements TypeInterface
-{
+class CurlPost implements TypeInterface {
 
-    public function sendPayload(Payload $payload)
-    {
+    public function sendPayload(Payload $payload) {
         $ch = curl_init($payload->getUrl());
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-        curl_setopt($ch, CURLOPT_HEADER, 1); 
+        curl_setopt($ch, CURLOPT_HEADER, 1);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
         if ($payload->getMethod() === 'post') {
-            curl_setopt($ch, CURLOPT_POST, 1);            
+            curl_setopt($ch, CURLOPT_POST, 1);
             curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($payload->getPostData()));
         }
 
         $headers = $payload->getHeaders();
-        if (!empty($headers)) {
+        if ( ! empty($headers)) {
             $curlHeaders = array();
             foreach ($headers as $key => $value) {
                 $curlHeaders[] = $key . ': ' . $value;
@@ -30,13 +28,11 @@ class CurlPost implements TypeInterface
         }
 
         $content = curl_exec($ch);
-        
-        $header_size    = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
-        $retHeaders     = substr($content, 0, $header_size);
-        $body           = substr($content, $header_size);
-        
+
+        $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+        $retHeaders = substr($content, 0, $header_size);
+        $body = substr($content, $header_size);
+
         return new CurlResponse(curl_getinfo($ch, CURLINFO_HTTP_CODE), $retHeaders, $body);
     }
-
-
 }
